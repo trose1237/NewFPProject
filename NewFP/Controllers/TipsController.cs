9b65a4dd637c6af3e61553cafdfeb8cb51bb2d65
trose1237 <85703591+trose1237@users.noreply.github.com>
2022-02-21 15:11:33 -0500

    created controllers

---
+++ NewFP/Controllers/TipsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using NewFP.Models;
+
+namespace NewFP.Controllers
+{
+    public class TipsController : Controller
+    {
+        private familypiDBEntities db = new familypiDBEntities();
+
+        // GET: Tips
+        public ActionResult Index()
+        {
+            return View(db.Tips.ToList());
+        }
+
+        // GET: Tips/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Tip tip = db.Tips.Find(id);
+            if (tip == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tip);
+        }
+
+        // GET: Tips/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Tips/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "TipID,Tip1")] Tip tip)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Tips.Add(tip);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(tip);
+        }
+
+        // GET: Tips/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Tip tip = db.Tips.Find(id);
+            if (tip == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tip);
+        }
+
+        // POST: Tips/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "TipID,Tip1")] Tip tip)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(tip).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(tip);
+        }
+
+        // GET: Tips/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Tip tip = db.Tips.Find(id);
+            if (tip == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tip);
+        }
+
+        // POST: Tips/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Tip tip = db.Tips.Find(id);
+            db.Tips.Remove(tip);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
