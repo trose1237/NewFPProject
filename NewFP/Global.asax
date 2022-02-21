e70b9a4f5b923f8e30b8758070ea9f3c2c67dbeb
trose1237 <85703591+trose1237@users.noreply.github.com>
2022-02-21 14:08:37 -0500

    Add project files.

---
+++ NewFP/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="NewFP.MvcApplication" Language="C#" %>
---
+++ NewFP/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(NewFP.Startup))]
+namespace NewFP
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
